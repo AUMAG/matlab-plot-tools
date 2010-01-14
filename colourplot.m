cbfaa37f65b38bf2c5b4d76a68ec7fc3eb860f9c
Will Robertson <wspr81@gmail.com>
2010-01-14 18:06:03 +1030

    Add "permute" feature

--- colourplot.m
+++ colourplot.m
@@ -1,16 +1,23 @@
 
-function [ RGBOUT ] = colourplot( series )
+function [ RGBOUT ] = colourplot( series , permute )
 %COLOURPLOT Make colourful, nice-looking plots
 %  This function takes the current figure and splits up the HSV colour
 %  space by the number of data lines there are. These colours are then
 %  converted to RGB and applied to the plot.
 %
+% COLOURPLOT(N)
 %  An optional argument specifies the number of times to use the
 %  colour space: e.g., colourplot(2) will turn, in a graph with 6 data
 %  series, the first and fourth plot blue, the second and fifth
 %  green, and the third and six red. The divisor of the number of
 %  plots and the number of colour space repetitions must be an
 %  integer.
+%
+% COLOURPLOT(N,PERMUTE)
+%  By default the lines are coloured in the order in which they
+%  were plot. This order can be changed by specifying a permutation
+%  of the order in the second argument, such as in a four-plot graph:
+%      colourplot(1,[1 3 2 4])
 %
 %  If the 'UserData' for a data line is 'colourplot:ignore', then
 %  it will not be included in the COLOURPLOT colouring.
@@ -23,7 +30,7 @@ function [ RGBOUT ] = colourplot( series )
 % this package at the development repository:
 %  <http://github.com/wspr/matlabpkg/>
 %
-% COLOURPLOT  v0.2  2009/22/06  Will Robertson
+% COLOURPLOT  v0.3  2010/13/01  Will Robertson
 % Licence appended.
 
 if nargin == 0
@@ -41,19 +48,27 @@ if round(Ncol) ~= Ncol
 end
 
 hsv = ones(Ncol,3);
-hsv(:,1) = ([1:Ncol]-1)'/Ncol;
+hsv(:,1) = ((1:Ncol)-1)'/Ncol;
 hsv(:,2) = 0.5;
 hsv(:,3) = 0.8;
 
 rgb = hsv2rgb(hsv);
 
+if nargin < 2
+  permute = 1:Nch;
+else
+  if ~isequal(sort(permute),1:Nch)
+    error('2nd argument must be a permutation of 1:N where N is the number of colours.');
+  end
+end
+
 if nargout == 0
   for ii = 1:Nch
     if isequal(get(ch(ii),'type'),'line')
-      set(ch(ii),'color',rgb(mod(ii-1,Ncol)+1,:))
+      set(ch(permute(ii)),'color',rgb(mod(ii-1,Ncol)+1,:))
     end
     if isequal(get(ch(ii),'type'),'surface')
-      set(ch(ii),'FaceColor',rgb(mod(ii-1,Ncol)+1,:),'EdgeColor',rgb(mod(ii-1,Ncol)+1,:))
+      set(ch(permute(ii)),'FaceColor',rgb(mod(ii-1,Ncol)+1,:),'EdgeColor',rgb(mod(ii-1,Ncol)+1,:))
     end
   end
 else
@@ -62,7 +77,7 @@ end
 
 return
 
-% Copyright (c) 2005-2009, Will Robertson, wspr 81 at gmail dot com
+% Copyright (c) 2005-2010, Will Robertson, wspr 81 at gmail dot com
 % All rights reserved.
 %
 % Distributed under the BSD licence in accordance with the wishes of the
