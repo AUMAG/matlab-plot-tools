506b7fa4d18c4bf9ed5072c9afe55fb15cddedc5
Will Robertson <wspr81@gmail.com>
2011-03-17 17:14:36 +1030

    "moreticks" and "lessticks": do what they say to plots

---
+++ moreticks.m
@@ -0,0 +1,18 @@
+function [] = moreticks(s,h,N)
+
+if nargin < 1
+  s = 'x';
+end
+if nargin < 2
+  h = gca;
+end
+if nargin < 3
+  N = 2;
+end
+
+ticks = get(gca,[s,'tick']);
+Nticks = length(ticks);
+
+ticks = linspace(ticks(1),ticks(end),N*Nticks-1);
+
+set(h,[s,'tick'],ticks);
