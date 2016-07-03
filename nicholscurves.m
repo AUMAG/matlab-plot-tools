6fc4865956ddbee721e8eba5620f2b33536c72bd
Will Robertson <will@wspr.io>
2016-07-03 22:00:24 +0930

    nicholscurves: updates from last exam

--- nicholscurves.m
+++ nicholscurves.m
@@ -11,8 +11,9 @@ function nicholscurves(varargin)
 %               appear. Default is [12 6 3 1 0.5 -0.5 -1 -3 -6 -10 -20 -40 -60].
 % 'linewidth' = Thickness of the lines on the plot. Default is 1.
 % 'frag'      = true or false; whether to use matlabfrab to print labels.
-% 'mlabels'   = true or false; whether to label the M circles
-% 'nlabels'   = true or false; whether to label the N circles
+% 'mlabels'   = true or false; whether to label the M circles.
+% 'nlabels'   = true or false; whether to label the N circles.
+% 'msat'      = "saturation" of M-circles, 0 (grey) to 1 (bright).
 
 % Copyright 2011 Will Robertson
 % Copyright 2011 Philipp Allgeuer
@@ -31,6 +32,7 @@ p.addParameter('nlabels',true);
 p.addParameter('mlabels',true);
 p.addParameter('mlabelangle',[210 210]);
 p.addParameter('axislabels',true);
+p.addParameter('msat',0.5);
 p.parse(varargin{:});
 
 % Save parsed arguments
@@ -42,9 +44,17 @@ LWidth=p.Results.linewidth;
 frag_bool = p.Results.frag;
 nlabels_bool = p.Results.nlabels;
 mlabels_bool = p.Results.mlabels;
-mlabelanglep = p.Results.mlabelangle(1);
-mlabelanglen = p.Results.mlabelangle(2);
+mlabelangle  = p.Results.mlabelangle;
+circles_sat  = p.Results.msat;
 
+if numel(mlabelangle) == 2
+  mlabelangle = nan(size(Gains));
+  mlabelangle(Gains>0) = p.Results.mlabelangle(1);
+  mlabelangle(Gains<0) = p.Results.mlabelangle(2);
+else
+  assert(numel(mlabelangle)==numel(Gains));
+end
+  
 % Freeze current plot
 hold_bool = false;
 if ishold
@@ -68,7 +78,7 @@ Ni_Ph=@(mdb,t) rad2deg(unwrap(angle(Ny(mdb,t))));
 Ni_Ga=@(mdb,t) 20.*log10(abs(Ny(mdb,t)));
 
 % Generate the colour space
-CalcRgb=@(mdb) hsv2rgb([((mdb-min(Gains))/(max(Gains)-min(Gains)))^1.5 0.5 0.8]);
+CalcRgb=@(mdb) hsv2rgb([((mdb-min(Gains))/(max(Gains)-min(Gains)))^1.5 circles_sat 0.8]);
 
 if frag_bool
   user_data = @(nn) ['matlabfrag:',...
@@ -79,30 +89,30 @@ else
   user_data = @(nn) '';
 end
 
+
+
 % Apply M-circle equations and plot the result
+c = 0;
 for i=Gains
+  c = c+1;
     PVals=Ni_Ph(i,0:0.1:360);
     GVals=Ni_Ga(i,0:0.1:360);
     for j=PCyc(1):PCyc(2)-1
-        plot(PVals+j*360,GVals,'color',CalcRgb(i),'linewidth',LWidth);
-        if mlabels_bool
-            if sign(i) > 0
-                mla = mlabelanglep;
-            else
-                mla = mlabelanglen;
-            end
-            if mla > 180
-                offset = (j+1)*360;
-            else
-                offset = j*360;
-            end
-            TextX=Ni_Ph(i,mla)+offset;
-            TextY=Ni_Ga(i,mla);
-            text(TextX,TextY,[num2str(i) 'dB'],...
-                'FontSize',LSize,..., ...
-                'horizontalalignment','center',...
-                'UserData',user_data(i));
+      plot(PVals+j*360,GVals,'color',CalcRgb(i),'linewidth',LWidth);
+      if mlabels_bool
+        mla = mlabelangle(c);
+        if mla > 180
+          offset = (j+1)*360;
+        else
+          offset = j*360;
         end
+        TextX=Ni_Ph(i,mla)+offset;
+        TextY=Ni_Ga(i,mla);
+        text(TextX,TextY,[num2str(i) 'dB'],...
+          'FontSize',LSize,..., ...
+          'horizontalalignment','center',...
+          'UserData',user_data(i));
+      end
     end
 end
 
