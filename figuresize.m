a366001cff734ecf52a6593e10cd2e1d2bca42be
Will Robertson <wspr81@gmail.com>
2019-05-19 13:38:35 +0930

    reorg figuresize()
    - add 'mm'
    - optional arguments work properly now
    - allow figuresize() by itself
    - add figuresize(M) weird shortcut (not sure if I will keep)
    - add fourth "padscale" argument

--- figuresize.m
+++ figuresize.m
@@ -1,39 +1,67 @@
-function figuresize( w , h , u )
+function figuresize( varargin )
 %FIGURESIZE Set a figure to a specific size
 %
 % When saving a figure as a PDF, it is necessary to set the
 % figure size appropriately. This function sets the "paper size"
 % sufficient that the figure is saved with a tight bounding box.
+% (10% extra white space surrounding the figure on each side.)
 % It will also set the figure size on screen correspondingly.
 %
-% figuresize(width,height) - sets the figure size in centimeters
-% figuresize(width,height,units) - sets the figure size in <units>
+% figuresize()
+%  - sets the figure size to 12cm width by 10cm height
 %
-% <units> can be any of the standard Matlab lengths.
+% figuresize(N)
+%  - SPECIAL CASE = figure(N); clf; hold on; figuresize()
 %
-% Will Robertson
-% 28 April 2010
+% figuresize(width,height)
+%  - sets the figure <width> and <height> in centimeters
 %
-% Copyright and licence information appended.
+% figuresize(width,height,units)
+%  - sets the figure size in <units>
+%  - <units> can be any of the standard Matlab length names,
+%    as well as 'mm', 'cm', 'in', or 'pt'. Default 'cm'.
+%
+% figuresize(w,h,u,padscale)
+%  - Use <padscale> to adjust (can be given as a single element or
+%    two-element vector for width/height separately). Default 0.01.
+
+
+if numel(varargin) == 0
+  figuresize(12,10,'cm')
+  return
+end
+
+if numel(varargin) == 1
+  figure(varargin{:}); clf; hold on
+  figuresize();
+  return
+end
+
+allowed_units = {'normalized','centimeters','inches','points','cm','in','pt','mm'};
 
 p = inputParser;
 p.addRequired('width', @(x) isnumeric(x) && all(size(x)==1) );
 p.addRequired('height',@(x) isnumeric(x) && all(size(x)==1) );
 p.addOptional('units','centimeters',...
-  @(x) any(strcmpi(x,{'normalized','centimeters','cm','inches','in','points','pt'})) );
+  @(x) any(strcmpi(x,allowed_units)) );
+p.addOptional('padscale',0.01, @(x) isnumeric(x) )
 
-p.parse( w, h, u );
+p.parse( varargin{:} );
 w = p.Results.width;
 h = p.Results.height;
 u = p.Results.units;
+s = p.Results.padscale;
+
+if numel(s) == 1
+  s = [s s];
+end
 
 switch u
   case 'cm', u = 'centimeters';
   case 'in', u = 'inches';
   case 'pt', u = 'points';
+  case 'mm', u = 'centimeters'; w = w/10; h = h/10;
 end
-  
-p = 0.01;
 
 set(gcf,'Units',u);
 screenpos = get(gcf,'Position');
@@ -41,13 +69,13 @@ screenpos = get(gcf,'Position');
 set(gcf,...
   'Position',[screenpos(1:2) w h],...
   'PaperUnits',u,...
-  'PaperPosition',[p*w p*h w h],...
-  'PaperSize',[w*(1+2*p) h*(1+2*p)]);
+  'PaperPosition',[s(1)*w s(2)*h w h],...
+  'PaperSize',[w*(1+2*s(1)) h*(1+2*s(2))]);
 
 end
 
 
-% Copyright (c) 2010 Will Robertson, wspr 81 at gmail dot com
+% Copyright (c) 2010-2019 Will Robertson, wspr 81 at gmail dot com
 % All rights reserved.
 %
 % Distributed under the BSD licence in accordance with the wishes of the
